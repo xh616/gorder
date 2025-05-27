5c0aa39aa1b2023d068c416821b6faf0d2b7ea6b
pumpkinXH <87806162+pumpkinXH@users.noreply.github.com>
2025-05-27 18:49:00 +0800

    优化服务等待

--- internal/common/client/grpc.go
+++ internal/common/client/grpc.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"context"
+	"errors"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"github.com/xh/gorder/internal/common/discovery"
@@ -10,9 +11,14 @@ import (
 	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"net"
+	"time"
 )
 
 func NewOrderGRPCClient(ctx context.Context) (client orderpb.OrderServiceClient, close func() error, err error) {
+	if !WaitForOrderGRPCClient(viper.GetDuration("dial-grpc-timeout") * time.Second) {
+		return nil, nil, errors.New("order grpc client not available")
+	}
 	grpcAddr, err := discovery.GetServiceAddr(ctx, viper.GetString("order.service-name"))
 	if err != nil {
 		return nil, func() error { return nil }, err
@@ -30,6 +36,9 @@ func NewOrderGRPCClient(ctx context.Context) (client orderpb.OrderServiceClient,
 }
 
 func NewStockGRPCClient(ctx context.Context) (client stockpb.StockServiceClient, close func() error, err error) {
+	if !WaitForStockGRPCClient(viper.GetDuration("dial-grpc-timeout") * time.Second) {
+		return nil, nil, errors.New("stock grpc client not available")
+	}
 	grpcAddr, err := discovery.GetServiceAddr(ctx, viper.GetString("stock.service-name"))
 	if err != nil {
 		return nil, func() error { return nil }, err
@@ -49,5 +58,45 @@ func grpcDialOpts(_ string) []grpc.DialOption {
 	return []grpc.DialOption{
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 		grpc.WithStatsHandler(otelgrpc.NewClientHandler()),
+	}
+}
+
+func WaitForOrderGRPCClient(timeout time.Duration) bool {
+	logrus.Infof("waiting for order grpc client, timeout: %v seconds", timeout.Seconds())
+	return waitFor(viper.GetString("order.grpc-addr"), timeout)
+}
+
+func WaitForStockGRPCClient(timeout time.Duration) bool {
+	logrus.Infof("waiting for stock grpc client, timeout: %v seconds", timeout.Seconds())
+	return waitFor(viper.GetString("stock.grpc-addr"), timeout)
+}
+
+// waitFor 等待直到端口可用
+func waitFor(addr string, timeout time.Duration) bool {
+	portAvailable := make(chan struct{})
+	timeoutCh := time.After(timeout)
+
+	go func() {
+		for {
+			select {
+			case <-timeoutCh:
+				return
+			default:
+				// continue
+			}
+			_, err := net.Dial("tcp", addr) //ping
+			if err == nil {                 // port is available
+				close(portAvailable) //关闭的时候自动返回0
+				return
+			}
+			time.Sleep(200 * time.Millisecond)
+		}
+	}()
+
+	select {
+	case <-portAvailable:
+		return true
+	case <-timeoutCh:
+		return false
 	}
 }
