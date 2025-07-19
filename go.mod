dcd5305f3f86cd88effbda3230396e640e447927
pumpkinXH <87806162+pumpkinXH@users.noreply.github.com>
2025-07-19 22:41:40 +0800

    Prometheus+Grafana

--- go.mod
+++ go.mod
@@ -2,16 +2,15 @@ module gorder
 
 go 1.23.6
 
+require github.com/prometheus/client_golang v1.22.0
+
 require (
-	github.com/go-logr/logr v1.4.2 // indirect
-	github.com/go-logr/stdr v1.2.2 // indirect
-	github.com/google/uuid v1.6.0 // indirect
-	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
-	go.opentelemetry.io/contrib/propagators/b3 v1.36.0 // indirect
-	go.opentelemetry.io/otel v1.36.0 // indirect
-	go.opentelemetry.io/otel/exporters/jaeger v1.17.0 // indirect
-	go.opentelemetry.io/otel/metric v1.36.0 // indirect
-	go.opentelemetry.io/otel/sdk v1.36.0 // indirect
-	go.opentelemetry.io/otel/trace v1.36.0 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/common v0.62.0 // indirect
+	github.com/prometheus/procfs v0.15.1 // indirect
 	golang.org/x/sys v0.33.0 // indirect
+	google.golang.org/protobuf v1.36.5 // indirect
 )
---
+++ prometheus/demo.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+const (
+	testAddr = "0.0.0.0:9123"
+)
+
+var httpStatusCodeCounter = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "http_status_code_counter",
+		Help: "Count http status code",
+	},
+	[]string{"status_code"},
+)
+
+func main() {
+	go produceData()
+	reg := prometheus.NewRegistry()
+	prometheus.WrapRegistererWith(prometheus.Labels{"serviceName": "demo-service"}, reg).MustRegister(
+		collectors.NewGoCollector(),                                       // Go runtime metrics
+		collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}), // Go process metrics
+		httpStatusCodeCounter,
+	)
+	// localhost:9123/metrics
+	http.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{}))
+	http.HandleFunc("/", sendMetricsHandler)
+	log.Fatal(http.ListenAndServe(testAddr, nil))
+}
+
+func sendMetricsHandler(w http.ResponseWriter, r *http.Request) {
+	var req request
+	defer func() {
+		//拿到的数据解析后暴露指标
+		httpStatusCodeCounter.WithLabelValues(req.StatusCode).Inc()
+		log.Printf("add 1 to %s", req.StatusCode)
+	}()
+	_ = json.NewDecoder(r.Body).Decode(&req)
+	log.Printf("receive req:%+v", req)
+	_, _ = w.Write([]byte(req.StatusCode))
+}
+
+type request struct {
+	StatusCode string
+}
+
+func produceData() {
+	codes := []string{"503", "404", "400", "200", "304", "500"}
+	for {
+		body, _ := json.Marshal(request{
+			StatusCode: codes[rand.Intn(len(codes))],
+		})
+		requestBody := bytes.NewBuffer(body)
+		//向localhost:9123/不停发数据
+		http.Post("http://"+testAddr, "application/json", requestBody)
+		log.Printf("send request=%s to %s", requestBody.String(), testAddr)
+		time.Sleep(2 * time.Second)
+	}
+}
